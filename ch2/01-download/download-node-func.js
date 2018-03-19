974a9e35c376f958482b9e8507a4b1f68127af0a
28kayak <kaya.ota@gmail.com>
2018-03-19 22:44:29 +0900

    add function call

--- ch2/01-download/download-node-func.js
+++ ch2/01-download/download-node-func.js
@@ -10,6 +10,15 @@ download(
 	function(){
 		console.log("Ok, soseki");
 	});//end of download 
+download(
+	"http://www.aozora.gr.jp/cards/000026/files/51334_50060.html",
+	"./downloaded_html/chuya_nakahara_kokyu_no_uta.html",
+	function() {
+		console.log("dl chuya_nakahara_kokyu_no_uta");
+	}
+
+
+	);
 
 //download a file located on the url to the direcotry 
 function download(url, savepath, callback){
