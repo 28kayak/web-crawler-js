08c2a80a0fe588ccca5b88cc70ac3ff57f1631a5
28kayak <kaya.ota@gmail.com>
2018-03-24 14:50:12 +0900

    update

--- ch2/03-getall/get_all.js
+++ ch2/03-getall/get_all.js
@@ -7,13 +7,26 @@ var path = require("path");
 
 //shared setting 
 var LINK_LEVEL = 3;
-var TARGET_URL = "http://nodejs.jp/nodejs.org_ja/docs/v0.10/api/";
+	// body...
+}
+//Nakahara Chuya
+//var TARGET_URL = "http://www.aozora.gr.jp/index_pages/person26.html"
+var TARGET_URL = "https://www.uta-net.com/songs/";
+//"http://nodejs.jp/nodejs.org_ja/docs/v0.10/api/";
+var list = {};
+var 
+
+downloadRec(TARGET_URL, 0);
+
 
 function downloadRec(url, level)
 {
+	console.log("Start Downloading : " + url);
 	//check level reaches to given 
 	if(level >= LINK_LEVEL)
 	{
+		return; 
+	}//end of if
 		//check if a file on the url is already downloaded or not 
 		if (list[url])
 		{
@@ -23,6 +36,7 @@ function downloadRec(url, level)
 		list[url] = true;
 		//ignore pages if they are not targeted 
 		var us = TARGET_URL.split("/");
+		console.log(us);
 		us.pop();
 		var base = us.join("/");
 		if(url.indexOf(base) < 0)
@@ -31,17 +45,60 @@ function downloadRec(url, level)
 		}
 		//get htmls
 		client.fetch(url, {}, function(err, $, res){
-			$("a").each(function(idx){
-				//get links that used in a-tags 
-				var href = $(this).attr('href');
-				
-			});
-
-		})
-
-
-
-
+			if (typeof $ === 'undefined')
+			{
+				//for some reasons, $ is undefined so skip it 
+				return; 
+			}
+			else
+			{
+				console.log("show $");
+				console.log($);
+				console.log("show $(a)");
+				console.log($("a"));
+				$("a").each(function(idx){
+					//get links that used in a-tags 
+					var href = $(this).attr('href');
+					if (!href) 
+					{
+						return;
+					}
+					//convert relative path to absolute path 
+					href = URL.resolve(url, href);
+					//ignore # 
+					href = href.replace(/\#.+$/, "");
+					downloadRec(href, level + 1);
+				});
+				//save pages 
+				//define file names 
+				if (url.substr(url.length-1, 1)== '/')
+				{
+					url += "index.html"; // automatically add index page 
+				}
+				var save_path = url.split("/").slice(2).join("/");
+				checkSaveDir(save_path);
+				console.log(save_path + "\n");
+				fs.writeFileSync(save_path, $.html());
+			}
+			
+		});//end of fetch
+	 
+}//end of downloadRec 
+function checkSaveDir(fname) {
+	//extract directory part 
+	var dir = path.dirname(fname);
+	//create directory recursively 
+	var dirlist = dir.split("/");
+	console.log("Print Dir List");
+	console.log(dirlist);
+	var p = "";
+	for (var i in dirlist)
+	{
+		p += dirlist[i] + "/";
+		if(!fs.existsSync(p))
+		{
+			fs.mkdirSync(p);
+		} 
 	}
 }
-
+	
